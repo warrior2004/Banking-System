d51789ecc97a719294c88be591199cc04542856c
Harshit Chauhan <harshit.honey003@gamil.com>
2026-03-24 17:23:53 +0530

    Added account API

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const authRouter = require("./routes/auth.route");
+
 
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
 
-/* POST /api/auth/register */
+/**
+ * - ROUTES
+ */
+const authRouter = require("./routes/auth.route");
+const accountRouter = require("./routes/account.routes")
+
+
+/**
+ * - USER ROUTES
+ */
 app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
 
 module.exports = app
---
+++ src/routes/account.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const authMiddleware = require("../middleware/auth.middleware")
+const accountController = require("../controllers/account.controller")
+
+
+const router = express.Router()
+
+/**
+ * - POST /api/accounts/
+ * - Create a new account
+ * * - Protected Route
+ */
+router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
+
+module.exports = router;
