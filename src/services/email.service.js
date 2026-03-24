8822d02386acde604e0fc8cec468aa5f7a8da87a
Harshit Chauhan <harshit.honey003@gamil.com>
2026-03-24 14:13:32 +0530

    Implemented the sending gmail logic

---
+++ src/services/email.service.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL_USER,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+});
+
+// Verify the connection configuration
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Error connecting to email server:', error);
+  } else {
+    console.log('Email server is ready to send messages');
+  }
+});
+
+// Function to send email
+const sendEmail = async (to, subject, text, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Banking System" <${process.env.EMAIL_USER}>`, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html, // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
+async function sendRegisterEmail(userEmail, name){
+    const subject = "Welcome to Backing System";
+    const text = `Hello ${name}, \n\n Thankyou for your registration at Backing System`;
+    const html = `<p>Hello ${name}, </p><p>Thankyou for registering at Banking System, we're excited to have you board!</p><p>Best reagard, <br>Banking System`
+    await sendEmail(userEmail, subject, text, html)
+}
+
+module.exports = {
+    sendRegisterEmail
+};
